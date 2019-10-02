4536089619fb225c927841e03af06ef78b0da782
marcorover <marcomorover@gmail.com>
2019-10-02 20:12:09 -0300

    atualizações gráfico estático

--- aplicacao/site-aplicacao/frontend/assets/js/dashboards.js
+++ aplicacao/site-aplicacao/frontend/assets/js/dashboards.js
@@ -4,46 +4,32 @@ var ctx = document.getElementById('chart-line').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ["01/03/2019","02/03/2019","03/03/2019","04/03/2019"],
         datasets: [{
-            label: '# of Votes',
+            label: 'Delta',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                
+                'rgba(54, 162, 235, 0.2)'
+               
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                
+                'rgba(54, 162, 235, 1)'
+                
             ],
             borderWidth: 1
         },
         {
-            label: '# of Votes',
+            label: 'Charlie',
             data: [15, 29, 30, 51, 21, 32],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)'
+                
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)'
+                
             ],
             borderWidth: 1
         }]
@@ -52,13 +38,15 @@ var myChart = new Chart(ctx, {
         responsive: true,
         title: {
             display: true,
-            text: 'Chart.js Line Chart'
+            text: 'Semanal'
         },
         tooltips: {
             mode: 'index',
             intersect: false,
+          
         },
         hover: {
+            backgroundColor : 'rgba(255, 99, 132, 0.2)',
             mode: 'nearest',
             intersect: true
         },
@@ -67,14 +55,14 @@ var myChart = new Chart(ctx, {
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Month'
+                    labelString: 'Período'
                 }
             }],
             yAxes: [{
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Value'
+                    labelString: 'Notificações'
                 }
             }]
         }
@@ -84,46 +72,38 @@ var ctz = document.getElementById('chart-bar').getContext('2d');
 var myChart1 = new Chart(ctz, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ["01/03/2019","02/03/2019","03/03/2019","04/03/2019"],
         datasets: [{
-            label: '# of Votes',
+            label: 'Delta',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
             ],
             borderWidth: 1
         },
         {
-            label: '# of Votes',
+            label: 'Charlie',
             data: [15, 29, 30, 51, 21, 32],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
             ],
             borderWidth: 1
         }]
@@ -132,7 +112,7 @@ var myChart1 = new Chart(ctz, {
         responsive: true,
         title: {
             display: true,
-            text: 'Chart.js Line Chart'
+            text: 'Semanal'
         },
         tooltips: {
             mode: 'index',
@@ -147,14 +127,14 @@ var myChart1 = new Chart(ctz, {
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Month'
+                    labelString: 'Período'
                 }
             }],
             yAxes: [{
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Value'
+                    labelString: 'Horas '
                 }
             }]
         }
@@ -164,17 +144,17 @@ var cty = document.getElementById('chart-pie').getContext('2d');
 var myChart1 = new Chart(cty, {
     type: 'pie',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ["Delta","Charlie"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)'   
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -186,27 +166,7 @@ var myChart1 = new Chart(cty, {
             ],
             borderWidth: 1
         },
-        {
-            label: '# of Votes',
-            data: [15, 29, 30, 51, 21, 32],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        ]
     },
     options: {
         responsive: true,
@@ -224,14 +184,14 @@ var myChart1 = new Chart(cty, {
         },
         scales: {
             xAxes: [{
-                display: true,
+                display: false,
                 scaleLabel: {
                     display: true,
                     labelString: 'Month'
                 }
             }],
             yAxes: [{
-                display: true,
+                display: false,
                 scaleLabel: {
                     display: true,
                     labelString: 'Value'
