68899e61785b77660d55a2647405c8c6f723cf84
Teodoro <joteodoro@deloitte.com>
2019-10-21 18:06:53 -0300

    alterado Modelo Lógico do Banco e login estatico do index

---
+++ Aplicação (Web)/public/js/login.js
@@ -0,0 +1,22 @@
+
+
+// function Efetuar_login() {
+//   var reg_name = getElementById('reg-senha');
+//   var reg_email = getElementById("reg-email");
+
+//   if (reg-name=="grupo.sys@gmail.com" && regemail=="1234") {
+//       window.location.href ='sistema/src/dashboard-project.html'
+//   }
+//   else {
+//     alert("Usuário inválido!")
+//   }
+// }
+
+function efetuar_login(nome, email) {
+    if (nome == "grupo.sys@gmail.com" && email == "1234") {
+        window.location.href = 'sistema/src/dashboard-project.html'
+    }
+    else {
+        alert("Usuário inválido!")
+    }
+}
