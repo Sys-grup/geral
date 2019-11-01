d01a03e64a3aa6357b92a06eba783bdaab2c3bd1
Aluno <henriquee.matos@hotmail.com>
2019-11-01 20:48:43 -0200

    Rotina de inserção de dados no banco feita

---
+++ Aplicação (Java)/PMSys/src/main/java/com/mycompany/pmsys/oshi/Executer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pmsys.oshi;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Executer {
+
+    public static void main(String[] args) throws Exception {
+        
+        JobDetail job = JobBuilder.newJob(OshiDados.class).build();													  //("0 0 10 1/1 * ? *") Produção
+        Trigger tarefa = (Trigger) TriggerBuilder.newTrigger().withIdentity("CronTrigger").withSchedule(CronScheduleBuilder.cronSchedule("0 0/5 * 1/1 * ? *")).build();
+        Scheduler sc = StdSchedulerFactory.getDefaultScheduler();
+        sc.start();
+        sc.scheduleJob(job, (org.quartz.Trigger) tarefa);
+        System.out.println("Iniciando envio de dados ao Banco");
+       
+    }
+}
--- Aplicação (Java)/PMSys/src/main/java/com/mycompany/pmsys/oshi/OshiDados.java
+++ Aplicação (Java)/PMSys/src/main/java/com/mycompany/pmsys/oshi/OshiDados.java
@@ -4,32 +4,58 @@
  * and open the template in the editor.
  */
 package com.mycompany.pmsys.oshi;
+
 /**
  *
  * @author Ultim
  */
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-@Component
-        
-public class OshiDados {
-    @Scheduled (cron= "0 0/1 * 1/1 * ? *")
-    public static void main(String[] args) {
-        
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+
+public class OshiDados implements Job{
+
+
+    public void execute(JobExecutionContext args0) throws JobExecutionException {
+
+    
+
         DadosCPU dCpu = new DadosCPU();
         DadosRAM dRam = new DadosRAM();
         DadosHD dHd = new DadosHD();
         DadosProcessos dProcessos = new DadosProcessos();
         
+        try{
+        System.out.println("Inserindo dados de CPU...");
         dCpu.insereDadosCPU();
-        
+        System.out.println("Inserindo dados de RAM...");
         dRam.insereDadosRam();
-        
+        System.out.println("Inserindo dadosde HD...");
         dHd.insereDadosHD();
-        
+        System.out.println("Enviando processos...");
         dProcessos.processosAtuais();
+        }catch(Exception e){
+            System.err.print("Erro ao inserir dados : " +e);
+        }
         
+        System.out.println("Dados Inseridos");
+
     }
     //Pega informações gerais do hardware do PC
-        
+
+
+
+
+
 }
+        
+        
