4b72e71c0b73b56436be26549803464ca0d1066b
Teodoro <joteodoro@deloitte.com>
2019-11-15 12:18:16 -0300

    adicionado modal squad

--- Aplicação (Web)/front/public/sistema/assets/js/squad/squad.js
+++ Aplicação (Web)/front/public/sistema/assets/js/squad/squad.js
@@ -1,8 +1,9 @@
 let listSquads =  [{"nome": "Alpha", "id": 1, "status":"atention"}, {"nome":"Beta", "id": 2, "status":"offline"}];
 let divs = `<div class="content col-md-2 " id= 'squad-0' >
-                <span class ="add" href="../../../src/cadastrar-squad.html">+</span>
+                <span class ="add" href="cadastrar-squad.html">+</span>
             </div>`;
 
+            
 listSquads.map(squad => {
         divs += `
             <div class="content col-md-2 ${squad.status}" id='squad-${squad.id}' >
