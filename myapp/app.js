4b6e8547823c89fce917f1eafda4fa88e412c10f
jsPedro2468 <joao.pedro2468@hotmail.com>
2019-11-24 11:51:42 -0200

    teste 4

--- myapp/app.js
+++ myapp/app.js
@@ -6,16 +6,16 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const systemRouter = require('./routes/system');
 
-const sistema = 'public/sistema'
+const sistema = 'public/sistema';
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/system', express.static(path.join(__dirname, sistema, 'src')));
 app.use('/assets', express.static(path.join(__dirname, sistema, 'assets')));
-app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/system', systemRouter);
